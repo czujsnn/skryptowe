bf3db75df34c3686b590121928adf9030c281a63
czujsnn <czajsonino@gmail.com>
2021-10-19 20:49:15 +0200

    from pc

---
+++ 2/1.py
@@ -0,0 +1,21 @@
+lancuch1 = '''Litwo! Ojczyzno moja! Ty jesteś jak zdrowie. Ile cię trzeba cenić, ten Bonapart figurka!
+Bez Suworowa to mówiąc, że przeszkadza kulturze, że ważny i w ulicę się w tem miejscu biegało domysłów tajnie!
+Więc szlachcic bez litości wsiedli spór był zwierzem szlacheckim,
+a on ekwipaż parskali ze śmiechu a u wniścia alkowy i ziemianinowi ustępować z Paryża'''
+
+lancuch2 = '''Drogi Marszałku, Wysoka Izbo. PKB rośnie. Z drugiej strony,
+wyeliminowanie korupcji rozszerza nam efekt odpowiednich warunków administracyjno-finansowych.
+Koleżanki i określenia nowych propozycji.
+Każdy już zapewne zdążył zauważyć iż dokończenie aktualnych projektów
+zmusza nas do przeanalizowania postaw uczestników wobec zadań stanowionych przez organizację.'''
+
+print((lancuch1+'\n'+lancuch2+'\n')*3, end='')
+
+lancuch = "Алексей Федорович Карамазов был приятелем. В особенности в комнату почти гонением."
+
+print(lancuch[0])
+print(lancuch[0:2])
+print(lancuch[2:])
+print(lancuch[-2])
+print(lancuch[-3:])
+print(lancuch[1::2])
