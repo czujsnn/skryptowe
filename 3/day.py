ca0f002df62b8f1108a5e341ac9efcbd432683c6
czujsnn <czajsonino@gmail.com>
2021-10-27 00:24:32 +0200

    touches to 1

--- 3/day.py
+++ 3/day.py
@@ -13,10 +13,30 @@ class Day(Enum):
     def difference(self, day):
 
         difference_days = day.value - self.value   
-        return difference_days + 7 if difference_days <= -4 else (difference_days - 7 if difference_days >= 4 else difference_days)
 
+        if difference_days <= -4:
+            return difference_days +7
+
+        elif difference_days >= 4:
+            return difference_days -7
+
+        else:
+            return difference_days
 
 def nthDayFrom(n, day):
 
     value = day.value + n
-    return Day(value + 7) if value <= 0 else (Day(value - 7) if value >= 8 else Day(value))
+
+    if value <= 0:
+
+        return Day(value + 7)
+
+    elif value >=8:
+
+        return Day(value - 7)
+
+    else:
+
+        return Day(value)
+
+
