b5dd57b8cd1f9f05f5d16293e8769e8fc9b65b7c
czujsnn <czajsonino@gmail.com>
2021-10-29 00:44:49 +0200

    .

--- 3/deanery_system/minitest.py
+++ 3/deanery_system/minitest.py
@@ -7,7 +7,7 @@ class Test_DSystem(unittest.TestCase):
     global term1, term2, term3
     term1 = Term(Day.TUE, 9, 45)
     term2 = Term(Day.WED, 10, 15)
-    term3 = Term(Day.TUE, 11, 15)
+    term3 = Term(Day.FRI, 16, 15)
 
     def test_earlier(self):
         self.assertEqual(term1.earlierThan(term2), True)
@@ -19,12 +19,12 @@ class Test_DSystem(unittest.TestCase):
         
     def test_equal(self):
         self.assertEqual(term1.equals(term2), False)
-        self.assertEqual(term1.equals(term3), False)
+        self.assertEqual(term2.equals(term3), False)
 
     def test_represent(self):
         self.assertEqual(term1.__str__(), "Wtorek 9:45 [90]")
         self.assertEqual(term2.__str__(), "Środa 10:15 [90]")
-        self.assertEqual(term3.__str__(), "Wtorek 11:15 [90]")
+        self.assertEqual(term3.__str__(), "Piątek 16:15 [90]")
 
     
 if __name__ == '__main__':
