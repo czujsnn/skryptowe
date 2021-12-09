b427dc17506ee15afe316144abd79e3e6167d964
Michał Czaja <czajsonino@gmail.com>
2021-12-09 22:43:20 +0100

    final

--- 8/2/script.js
+++ 8/2/script.js
@@ -1,66 +1,124 @@
-"use strict"                    //tryb scisly dla calego skryptu
+("use strict");
 var expect = chai.expect;
 
-let inp = window.prompt('Data');
-let total = 0;
-while (inp != null){
-    console.log(inp);
-    total += sum(inp);
-    console.log("\t"+numbers(inp)+"\t"+chars(inp)+"\t"+ total);
-    inp = window.prompt('Data');
+function cyfry(napis) {
+  var numbers = napis.match(/\d/g);                //any digit global
+  var sum = 0;
+
+  if (numbers === null) {
+    return 0;
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += parseInt(numbers[i]);
+  }
+
+  return sum;
 }
 
-function numbers(string){
-    let sum = 0;
-    let num = string.match(/[0-9]+/g);
-    if (num != null) {num.map(function(x){return String(x).split('')}).map(function(n){n.forEach(num => {sum += parseInt(num)})});}
-    return sum;
+function litery(napis) {
+  var litery = napis.match(/[a-z]/gi);          //a-z case sensitive global
+
+  if (litery === null) {
+    return 0;
+  }
+
+  return litery.length;
 }
 
-function chars(string){
-    let sum = 0;
-    let num = string.match(/[^0-9]+/g)
-    if (num != null){ num.map(function(x){return x.split('')}).map(function(x){x.forEach(c => {sum += 1})});}
-    return sum;
+function sum(napis) {
+
+  if (napis.match(/^\d+/g)){
+    add_for_sum += parseInt(napis.match(/^\d+/g))                    //jezeli napis zaczyna sie od cyfry, potem cokolwiek, globalny
+  }
+
+  return add_for_sum;
 }
 
-function sum(string){
-    return isNaN(parseInt(string)) ? 0 : parseInt(string);
-}
+var add_for_sum = 0;
+do {
+  input = window.prompt("Podaj napis");
 
+  if (input != null){
+    console.log(`\t ${cyfry(input)} \t ${litery(input)} \t ${sum(input)}`);                 //output as specified in ex.
+  }
 
-describe('The chars() function', function(){
-    it('Returns 3 for a123b4342c', function(){
-        expect(chars('a123b4342c')).to.equal(3);
-    });
-    it('Returns 9 for 0azsdfetr1a', function(){
-        expect(chars('0azsdfetr1a')).to.equal(9);
-    });
-    it('Returns 0 for 01321312', function(){
-        expect(chars('01321312')).to.equal(0);
-    });
+} while (input != null);
+
+describe("Funkcja cyfry()", function () {       //cyfry test
+  it("Returns 6 for 123", function () {
+    expect(cyfry("123")).to.equal(6);
+  });
+
+  it("Returns 5 for ascijo^$@1SA111ASDHU^H$#*(SC(VC)S$^1", function () {
+    expect(cyfry("ascijo^$@1SA111ASDHU^H$#*(SC(VC)S$^1")).to.equal(5);
+  });
+
+  it("Returns 0 for rpotorkeytovpdspokpsod", function () {
+    expect(cyfry("rpotorkeytovpdspokpsod")).to.equal(0);
+  });
+
+  it("Returns 6 for 123IERIJIOI$#J%(", function () {
+    expect(cyfry("123IERIJIOI$#J%(")).to.equal(6);
+  });
+
+  it("Returns 0 for empty input", function () {
+    expect(cyfry("")).to.equal(0);
+  });
+
 });
 
-describe('The numbers() function', function(){
-    it('Returns 3 for 111', function(){
-        expect(numbers('111')).to.equal(3);
-    })
-    it('Returns 0 for asdbvclkj', function(){
-        expect(numbers('asdbvclkj')).to.equal(0);
-    })
-    it('Returns 15 for 5sadasda4dsa1as32', function(){
-        expect(numbers('5sadasda4dsa1as32')).to.equal(15);
-    })
-})
+describe("Funkcja litery()", function () {      //litery test
+  it("Returns 0 for 1234567890", function () {
+    expect(litery("1234567890")).to.equal(0);
+  });
+  it("Returns 6 for ABC123456789!@#$EFG", function () {
+    expect(litery("ABC123456789!@#$EFG")).to.equal(6);
+  });
+  it("Returns 8 for abcdABCD", function () {
+    expect(litery("abcdABCD")).to.equal(8);
+  });
 
-describe('All functions for blank string', function(){
-    it('Blank for numbers()', function(){
-        expect(numbers('')).to.equal(0);
-    })
-    it('Blank for chars()', function(){
-        expect(chars('')).to.equal(0);
-    })
-    it('Blank for sum()', function(){
-        expect(sum('')).to.equal(0);
-    })
-})
+  it("Returns 2 for 1AB2191239032-23$(#!@", function () {
+    expect(litery("1AB2191239032-23$(#!@")).to.equal(2);
+  });
+
+  it("Returns 0 for empty input", function () {
+    expect(litery("")).to.equal(0);
+  });
+
+});
+
+describe("Funkcja suma()", function () {        //suma test
+  before(function () {                  //runs before all tests in this file
+    var add_for_sum = 0;
+  });
+  beforeEach(function () {              //runs before each test in this block
+    add_for_sum = 0;
+  });
+  afterEach(function () {               //runs after each test in this block - "reseting" add_for_sum
+    add_for_sum = 0;
+  });
+
+  it("Returns 0 for empty input", function () {
+    sum("");
+    expect(add_for_sum).to.equal(0);
+  });
+
+  it("Returns 111 for 111", function () {
+    sum("111");
+    expect(add_for_sum).to.equal(111);
+  });
+
+  it("Returns 111 for 111 and !111 afterwards", function () {
+    sum("111");
+    sum("!111");
+    expect(add_for_sum).to.equal(111);
+  });
+
+  it("Returns 222 for 111 and 111!@#$%ABCDE afterwards", function () {
+    sum("111");
+    sum("111!@#$%ABCDE");
+    expect(add_for_sum).to.equal(222);
+  });
+});
