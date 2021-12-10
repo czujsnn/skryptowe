2d535b2c2607b6417ce55ea58e43d7871797fc96
Michał Czaja <czajsonino@gmail.com>
2021-12-10 10:12:02 +0100

    .

--- 8/1/script.js
+++ 8/1/script.js
@@ -1,11 +1,12 @@
 window.onload = function(){
     console.log('Tekst 1');
-    window.alert('Tekst 2')
+    window.alert('Tekst 2');
 }
 
 document.getElementById('btnClick').onclick = function() {
     let form_input = document.forms[0].elements;
     document.getElementById('output').innerHTML = "<b>Value1 :</b> " + form_input[0].value + "<br><b>Value2 :</b> " + form_input[1].value;
+    
     console.log(typeof(form_input[0].value))
     console.log(typeof(form_input[1].value))
 
