79331fada7c0c912eb18ff667e911874acafb9e2
Michał Czaja <czajsonino@gmail.com>
2021-12-15 22:11:18 +0100

    final 17

---
+++ 9/1/script.js
@@ -0,0 +1,7 @@
+document.getElementById('del').onclick = function () {
+    document.styleSheets[0].disabled = true;
+}
+
+document.getElementById('set').onclick = function () {
+    document.styleSheets[0].disabled = false;
+}
